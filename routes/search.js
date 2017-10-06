275f765f6d5e083e3675c36475ed512638328f45
CiprianPintilie <pintilie.c.a@gmail.com>
2017-10-06 21:31:07 +0200

    Change - add route to force the user to search both a start date AND a end date at once, and route to search all houses

--- routes/search.js
+++ routes/search.js
@@ -3,16 +3,37 @@ var router = express.Router();
 var dbContext = require('../data/databaseManager.js');
 
 /* GET search results. */
-router.get('/:location', function (req, res, next) {
+router.get('/', function (req, res, next) {
     let city = req.params.location;
     res.type('application/json');
     res.send(dbContext.getHouses(
-        city, req.query.beds,
+        null,
+        req.query.beds,
+        req.query.priceMin,
+        req.query.priceMax));
+});
+
+router.get('/:location', function (req, res, next) {
+    res.type('application/json');
+    res.send(dbContext.getHouses(
+        req.params.location, 
+        req.query.beds,
+        req.query.priceMin,
+        req.query.priceMax));
+});
+
+router.get('/:location/:startDate/:endDate', function (req, res, next) {
+    let city = req.params.location;
+    res.type('application/json');
+    if (Date.parse(req.params.startDate) > Date.parse(req.params.endDate))
+        res.send("Start date must be inferior or equal to end date");
+    res.send(dbContext.getHouses(
+        city, 
+        req.query.beds,
         req.query.priceMin,
         req.query.priceMax,
-        req.query.startDate,
-        req.query.endDate));
-    
+        req.params.startDate,
+        req.params.endDate));
 });
 
 module.exports = router;
