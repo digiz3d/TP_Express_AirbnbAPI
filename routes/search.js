a9b28e98cb4aa10a82c2963628ca3fef616031d3
CiprianPintilie <pintilie.c.a@gmail.com>
2017-10-06 20:05:35 +0200

    Change - refactoring and adding query strings

--- routes/search.js
+++ routes/search.js
@@ -3,9 +3,16 @@ var router = express.Router();
 var dbContext = require('../data/databaseManager.js');
 
 /* GET search results. */
-router.get('/:location', function(req, res, next) {
+router.get('/:location', function (req, res, next) {
     let city = req.params.location;
-    res.send(dbContext.getHousesByCity(city));
+    res.type('application/json');
+    res.send(dbContext.getHouses(
+        city, req.query.beds,
+        req.query.priceMin,
+        req.query.priceMax,
+        req.query.startDate,
+        req.query.endDate));
+    
 });
 
 module.exports = router;
