cb897b28efd1ea8afc98f7e24e7050072d3886bd
CiprianPintilie <pintilie.c.a@gmail.com>
2017-10-06 20:02:36 +0200

    Change - Add JSON formatting to app

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ app.use('/users', users);
 app.use('/login', login);
 app.use('/search', search)
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -47,5 +47,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.set('json spaces', '\t');
 
 module.exports = app;
