0a494beb33ac908dd4ad4356953ecc167032a92c
CiprianPintilie <pintilie.c.a@gmail.com>
2017-10-09 23:00:18 +0200

    Change - auto mailing when booked

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var dbContext = require('../data/databaseManager.js');
+var mailer = require('../mailer.js');
 
 /* GET bookings */
 router.get('/', function(req, res) {
@@ -10,9 +11,19 @@ router.get('/', function(req, res) {
 /* create new booking */
 router.get('/:id/:startDate/:endDate', function(req, res) {
     if (dbContext.getHouseById(req.params.id) != null) {
-        if (!dbContext.isHouseBooked(req.params.id, req.params.startDate, req.params.endDate)) {
+        if (!dbContext.isHouseBooked(req.params.id, req.pareq.params.idrams.startDate, req.params.endDate)) {
             dbContext.setHouseBooked(req.params.id, req.params.startDate, req.params.endDate)
             res.send("Housing booked :)");
+            transport.sendMail(mailer.getMessage("leandre.daumont@gmail.com", "House booked !", "Congrats, you just booked the house n°" + 
+            req.params.id + " from " + req.params.startDate + " to " + req.params.endDate), function(error, info){
+                if(error){
+                    res.status(500);
+                    res.send("rip : "+error);
+                }
+                else {
+                    res.send("mail send to "+req.params.to);
+                }
+            });
         } else {
             res.status(409);
             res.send("Housing already booked at this date. Change the dates and try again.");
